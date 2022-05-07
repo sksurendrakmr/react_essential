9da8f3f1671d6fc4b08640ab3ddfee9fb40f7248
sksurendrakmr <sksurendrakmr@gmail.com>
2022-05-07 14:22:16 +0530

    Configure Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Dashboard } from './components/code-splitting/Dashboard';
+import { Home } from './components/code-splitting/Home';
+import { Profile } from './components/code-splitting/Profile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </>)
 }
 
 export default App;
