ff8b8818624b62de51e1732fedb29f4e8907fd4f
sksurendrakmr <sksurendrakmr@gmail.com>
2022-06-17 14:38:04 +0530

    Render props Intro1

--- src/components/rendering-props/AppRenderProps.tsx
+++ src/components/rendering-props/AppRenderProps.tsx
@@ -4,6 +4,12 @@ import { User } from './user'
 /**
  * Why??
  * To share the functionality between react component.
+ * 
+ * With render props pattern, we can use a props whose value is a function to
+ * control what is actually render by the component.
+ * 
+ * The term render prop refers to a technique for sharing code between react components
+ * using a prop whose value is a function.
  */
 
 export const AppRenderProps = () => {
